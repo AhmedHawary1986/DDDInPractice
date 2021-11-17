1ed4270027c0210be082bb541b4aa93c7f7668e1
Ahmed Elhawary <Ahmed.Elhawary@integrant.com>
2021-11-17 11:38:30 +0200

    Second Commit

---
+++ DDDInPractice.Logic/SnackMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDInPractice.Logic
+{
+    public sealed class SnackMachine
+    {
+        public int OneCentCount { get; private set; }
+        public int FiveCentCount { get; private set; }
+
+        public int QuarterCount { get; private set; }
+
+        public int OneDollarCount { get; private set; }
+
+        public int FiveDollarCount { get; private set; }
+
+        public int TwentyDollarCount { get; private set; }
+    }
+}
